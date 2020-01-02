16be728e9223c7b2de4c1d927423db7ee45447bd
yuvalkit <58812094+yuvalkit@users.noreply.github.com>
2020-01-02 13:08:34 +0200

    Add files via upload

--- Commands/ConnectCommand.cpp
+++ Commands/ConnectCommand.cpp
@@ -10,22 +10,25 @@
 
 using namespace std;
 
-//connect the simulator's socket, and the connect again and stay connected for sending data
+// connect the simulator's socket, and the connect again and stay connected for sending data
 int ConnectCommand::execute(vector<string> strings, int index) {
     DataManager* manager = DataManager::getInstance();
     int returnIndex = index + 1;
     this->ip = strings[returnIndex];
+    // get the expression of the port and then calculate it
     Expression* exp = manager->getInter()->interpret(strings[returnIndex + 1]);
     this->port = (int) exp->calculate();
     delete exp;
+    // blocking call of the client connection
     thread clientThread(&ConnectCommand::connectToSimulator, this);
     clientThread.join();
+    // does the send to simulator thread in the background
     manager->clientThread = thread(sendToSimulator);
     // increase returned index by 1 for next
     return returnIndex + 2;
 }
 
-//skip over the connection
+// skip over the connection
 int ConnectCommand::dummyExecute(vector<string> strings, int index) {
     if (strings.empty()) {
         cerr << "There are no strings"<<endl;
@@ -34,10 +37,10 @@ int ConnectCommand::dummyExecute(vector<string> strings, int index) {
     return index + 3;
 }
 
-//connect the simulator's socket, and save the socket number
+// connect the simulator's socket, and save the socket number
 void ConnectCommand::connectToSimulator() {
     DataManager* manager = DataManager::getInstance();
-    //create socket
+    // create socket
     manager->changeClientSocket(socket(AF_INET, SOCK_STREAM, 0));
     if (manager->getClientSocket() == -1) {
         //error
@@ -45,13 +48,13 @@ void ConnectCommand::connectToSimulator() {
         return;
     }
 
-    //We need to create a sockaddr obj to hold address of server
+    // we need to create a sockaddr obj to hold address of server
     sockaddr_in address{}; //in means IP4
-    address.sin_family = AF_INET;//IP4
-    address.sin_addr.s_addr = inet_addr(this->ip.c_str());  //the localhost address
-    address.sin_port = htons(this->port);
+    address.sin_family = AF_INET; //IP4
+    address.sin_addr.s_addr = inet_addr(this->ip.c_str()); //get the ip
+    address.sin_port = htons(this->port); // get the ip
 
-    // Requesting a connection with the server on local host with the relevant port
+    // requesting a connection with the server
     int is_connect = connect(manager->getClientSocket(), (struct sockaddr *)&address, sizeof(address));
     if (is_connect == -1) {
         cerr << "Could not connect to host server" << endl;
@@ -59,13 +62,16 @@ void ConnectCommand::connectToSimulator() {
     }
 }
 
-//connect the simulator's socket for sending data to simulator
+// connect the simulator's socket for sending data to simulator
 void ConnectCommand::sendToSimulator() {
     DataManager* manager = DataManager::getInstance();
     string data;
+    // until the program ends
     while (!manager->isDone()) {
+        // if there is data to send
         if (manager->getSending()) {
             data = manager->getStringToSend();
+            // send to simulator
             int is_sent = send(manager->getClientSocket() , data.c_str(), data.size(), 0);
             if (is_sent == -1) {
                 cerr << "Error sending message" << endl;
