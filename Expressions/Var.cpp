7dbafc332ee2ed6c3394a2d6b4c92985c70b4b8c
giladby <giladby11@gmail.com>
2020-01-02 13:52:46 +0200

    update

--- Expressions/Var.cpp
+++ Expressions/Var.cpp
@@ -18,7 +18,7 @@ Var::Var(double inputVal, bool inputHasNameFlag) {
     this->hasNameFlag = inputHasNameFlag;
 }
 
-//Ctor of variable from simulator
+//Ctor of variable of simulator
 Var::Var(string inputSim, bool inputRecv) {
     this->sim = inputSim;
     this->recv = inputRecv;
@@ -26,32 +26,40 @@ Var::Var(string inputSim, bool inputRecv) {
     this->hasNameFlag = true;
 }
 
+//get the sim string
 string Var::getSim() {
     return this->sim;
 }
 
+//get the value the variable holds
 double Var::getFlatValue() {
     return  this->val;
 }
 
+//calculate the variable value
 double Var::calculate() {
     DataManager *m = DataManager::getInstance();
     double value;
+    //if this variable isn't in map
     if(this->sim == "") {
         return this->getFlatValue();
     }
+    //search the relevant value in map
     value = m->getMapValue(this->sim);
     return value;
 }
 
+// this var value has to be updated by the simulator
 bool Var::isRecieving() {
     return this->recv;
 }
 
+//change the value the variable holds
 void Var::setValue(double newVal) {
     this->val = newVal;
 }
 
+//check if the variable ahs name
 bool Var::hasName() {
     return this->hasNameFlag;
 }
